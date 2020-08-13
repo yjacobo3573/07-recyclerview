bc56fddd644b8edc42d232e5ae7238f5794c44a8
irconde <irconde@ualr.edu>
2020-08-13 11:07:38 -0500

    Use view binding to reference layour components

--- app/src/main/java/com/ualr/recyclerview/MainActivity.java
+++ app/src/main/java/com/ualr/recyclerview/MainActivity.java
@@ -8,19 +8,21 @@ import android.os.Bundle;
 
 import com.ualr.recyclerview.adapter.AdapterListBasic;
 import com.ualr.recyclerview.data.DataGenerator;
+import com.ualr.recyclerview.databinding.ActivityMainBinding;
 import com.ualr.recyclerview.model.People;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView recyclerView;
+    private ActivityMainBinding mBinding;
     private AdapterListBasic mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
         initRecyclerView();
     }
 
@@ -31,17 +33,15 @@ public class MainActivity extends AppCompatActivity {
         items.addAll(DataGenerator.getPeopleData(this));
 
         // TODO 07: Get a reference to the RecyclerView
-        recyclerView = findViewById(R.id.recyclerView);
-
         // TODO 08: Create a new LayoutManager and plug it into the RecyclerView
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
+        mBinding.recyclerView.setLayoutManager(layoutManager);
 
         // TODO 09: Create a new adapter class: AdapterListBasic class
 
         // TODO 22: Instantiate the adapter (pass its data source) and plug the new instance in the recyclerView
         mAdapter = new AdapterListBasic(this, items);
-        recyclerView.setAdapter(mAdapter);
+        mBinding.recyclerView.setAdapter(mAdapter);
 
     }
 }
