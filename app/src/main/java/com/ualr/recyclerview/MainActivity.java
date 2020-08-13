2a5962b4f4bd871d04196c472c76709c67a98e0a
irconde <irconde@ualr.edu>
2020-08-13 11:07:38 -0500

    Use view binding to reference layour components

--- app/src/main/java/com/ualr/recyclerview/MainActivity.java
+++ app/src/main/java/com/ualr/recyclerview/MainActivity.java
@@ -5,34 +5,29 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.ualr.recyclerview.adapter.AdapterListBasic;
 import com.ualr.recyclerview.data.DataGenerator;
 import com.ualr.recyclerview.model.Item;
-import com.ualr.recyclerview.model.People;
-import com.ualr.recyclerview.model.SectionHeader;
-import com.ualr.recyclerview.utils.Tools;
+import com.ualr.recyclerview.databinding.ActivityMainBinding;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     private static final int DEFAULT_POS = 2;
 
-    private RecyclerView recyclerView;
+    private ActivityMainBinding mBinding;
     private AdapterListBasic mAdapter;
     private List<Item> mDataSource;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
         initRecyclerView();
     }
 
@@ -40,12 +35,9 @@ public class MainActivity extends AppCompatActivity {
         mDataSource = DataGenerator.getPeopleData(this);
 
         mAdapter = new AdapterListBasic(this, mDataSource);
-
-        recyclerView = findViewById(R.id.recyclerView);
-        recyclerView.setAdapter(mAdapter);
-
+        mBinding.recyclerView.setAdapter(mAdapter);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
+        mBinding.recyclerView.setLayoutManager(layoutManager);
 
         // TODO 05: Invoke the removeItem method
         mAdapter.setOnItemClickListener(new AdapterListBasic.OnItemClickListener() {
@@ -54,8 +46,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        FloatingActionButton addBtn = findViewById(R.id.floating_action_button);
-        addBtn.setOnClickListener(new View.OnClickListener() {
+        mBinding.floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // TODO 09: Invoke adapter's addItem method to add new item to the recyclerview
