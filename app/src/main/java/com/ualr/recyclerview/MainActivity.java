8938eb233b19959d05f63c0bc5ccd043340707e9
irconde <irconde@ualr.edu>
2020-08-13 11:07:38 -0500

    Use view binding to reference layour components

--- app/src/main/java/com/ualr/recyclerview/MainActivity.java
+++ app/src/main/java/com/ualr/recyclerview/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 
 import com.ualr.recyclerview.adapter.AdapterListBasic;
 import com.ualr.recyclerview.data.DataGenerator;
+import com.ualr.recyclerview.databinding.ActivityMainBinding;
 import com.ualr.recyclerview.model.People;
 
 import java.util.List;
@@ -17,14 +18,14 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
-
-    private RecyclerView recyclerView;
+    private ActivityMainBinding mBinding;
     private AdapterListBasic mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
         initRecyclerView();
     }
 
@@ -34,12 +35,10 @@ public class MainActivity extends AppCompatActivity {
         items.addAll(DataGenerator.getPeopleData(this));
 
         mAdapter = new AdapterListBasic(this, items);
-
-        recyclerView = findViewById(R.id.recyclerView);
-        recyclerView.setAdapter(mAdapter);
+        mBinding.recyclerView.setAdapter(mAdapter);
 
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
+        mBinding.recyclerView.setLayoutManager(layoutManager);
 
         // TODO 05: We plug a new listener in the adapter
         mAdapter.setOnItemClickListener(new AdapterListBasic.OnItemClickListener() {
